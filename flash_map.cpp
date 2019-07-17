20bcab53f73a15ad72e6b01751ef8591c300e3ab
George Beckstein <donbeckstein@live.com>
2019-07-16 22:08:02 -0400

    Tested updates from secondary partition

--- flash_map.cpp
+++ flash_map.cpp
@@ -7,12 +7,15 @@
 
 #include "BlockDevice.h"
 #include "QSPIFBlockDevice.h"
+#include "SlicingBlockDevice.h"
 
 QSPIFBlockDevice qspi_bd(QSPI_FLASH1_IO0, QSPI_FLASH1_IO1, QSPI_FLASH1_IO2, QSPI_FLASH1_IO3,
 		QSPI_FLASH1_SCK, QSPI_FLASH1_CSN, QSPIF_POLARITY_MODE_0, MBED_CONF_QSPIF_QSPI_FREQ);
 
-mbed::BlockDevice* mcuboot_secondary_bd = (mbed::BlockDevice*) &qspi_bd;
+SlicingBlockDevice raw_binary_bd(&qspi_bd, 0x200000, (0x200000 + 819200));
+
+mbed::BlockDevice* mcuboot_secondary_bd = (mbed::BlockDevice*) &raw_binary_bd;
 
 void mbed_mcuboot_user_init(void) {
-	qspi_bd.init();
+	mcuboot_secondary_bd->init();
 }
