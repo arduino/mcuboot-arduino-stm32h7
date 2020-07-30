45b07d615354532db0e3ee0fc2463acdf7249d24
George Beckstein <donbeckstein@live.com>
2020-07-30 13:05:23 -0400

    Changed secondary_bd declaration to use new default BlockDevice API

--- flash_map.cpp
+++ flash_map.cpp
@@ -6,15 +6,8 @@
  */
 
 #include "BlockDevice.h"
-#include "QSPIFBlockDevice.h"
-#include "SlicingBlockDevice.h"
 
-QSPIFBlockDevice qspi_bd(QSPI_FLASH1_IO0, QSPI_FLASH1_IO1, QSPI_FLASH1_IO2, QSPI_FLASH1_IO3,
-		QSPI_FLASH1_SCK, QSPI_FLASH1_CSN, QSPIF_POLARITY_MODE_0, MBED_CONF_QSPIF_QSPI_FREQ);
-
-SlicingBlockDevice raw_binary_bd(&qspi_bd, 0x200000, (0x200000 + 819200));
-
-mbed::BlockDevice* mcuboot_secondary_bd = (mbed::BlockDevice*) &raw_binary_bd;
+mbed::BlockDevice* mcuboot_secondary_bd = mbed::BlockDevice::get_default_instance();
 
 void mbed_mcuboot_user_init(void) {
 	mcuboot_secondary_bd->init();
