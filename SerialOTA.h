fe3ba3a18003210e64d1867e42c117693c11eac4
George Beckstein <donbeckstein@live.com>
2020-07-30 17:07:48 -0400

    Creating serial OTA example

---
+++ SerialOTA.h
@@ -0,0 +1,21 @@
+/*
+ * SerialOTA.h
+ *
+ *  Created on: Jul 30, 2020
+ *      Author: gdbeckstein
+ */
+
+#ifndef SERIALOTA_H_
+#define SERIALOTA_H_
+
+#include "Stream.h"
+
+class SerialOTA
+{
+public:
+
+    SerialOTA();
+
+};
+
+#endif /* SERIALOTA_H_ */
