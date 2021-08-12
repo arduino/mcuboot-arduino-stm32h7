ce4a14a07dd7506bec820b1346cbc1825a3ee139
pennam <m.pennasilico@arduino.cc>
2021-08-12 11:21:13 +0200

    Change default storage type to SD_FATFS

--- envie/ota.cpp
+++ envie/ota.cpp
@@ -5,7 +5,7 @@ int setOTAData(enum storageType storage_type, uint32_t data_offset, uint32_t upd
 	return 0;
 }
 int getOTAData(enum storageType* storage_type, uint32_t* data_offset, uint32_t* update_size) {
-  *storage_type = QSPI_FLASH_FATFS_MBR;	
+  *storage_type = SD_FATFS;	
   *data_offset = 2;
   *update_size = MCUBOOT_SLOT_SIZE;
   return 0;	
