f926c2d01578163f81d5b6775768015fc4bc61ad
pennam <m.pennasilico@arduino.cc>
2021-08-30 09:49:47 +0200

    Moved default block device code in ota files

--- default_bd.cpp
+++ default_bd.cpp
@@ -5,24 +5,10 @@
  *      Author: gdbeckstein
  */
 
+#if !MCUBOOT_AS_ENVIE
+
 #include "BlockDevice.h"
 #include "SlicingBlockDevice.h"
-#include "FlashIAPBlockDevice.h"
-#include "bootutil/bootutil_log.h"
-
-#if MCUBOOT_AS_ENVIE || MCUBOOT_USE_FILE_BD
-#include "MBRBlockDevice.h"
-#include "FileBlockDevice.h"
-#include "FATFileSystem.h"
-#endif
-
-#if MCUBOOT_AS_ENVIE
-#include "SDMMCBlockDevice.h"
-#if MCUBOOT_ENVIE_LITTLEFS
-#include "LittleFileSystem.h"
-#endif
-#include "ota.h"
-#endif
 
 #if COMPONENT_SPIF
 #include "SPIFBlockDevice.h"
@@ -44,54 +30,8 @@ const spi_pinmap_t static_spi_pinmap = get_spi_pinmap(MBED_CONF_SD_SPI_MOSI, MBE
 #endif
 #endif
 
-/*
- * MCUBOOT_AS_ENVIE     -> Secondary Block device defined by getOTAData [SDCARD, QSPI] Internal flash not supported
- *                      -> !!WARNING!! Scratch Block device by default on QSPI + MBR + FAT
- *
- * MCUBOOT_USE_FILE_BD  -> Secondary Block device on QSPI + MBR + FAT
- *                      -> Scratch Block device on QSPI + MBR + FAT
- *
- *                      -> Secondary Block device on RAW QSPI @0x0000000
- *                      -> Scratch Block device on Internal flash @MCUBOOT_SCRATCH_START_ADDR
- */
-
 BlockDevice *BlockDevice::get_default_instance()
 {
-#if MCUBOOT_AS_ENVIE
-    storageType storage_type;
-    uint32_t data_offset;
-    uint32_t update_size;
-
-    getOTAData(&storage_type, &data_offset, &update_size);
-
-    //if (storage_type & INTERNAL_FLASH_FLAG) {
-    //    if (storage_type & (FATFS_FLAG | LITTLEFS_FLAG)) {
-    //        // have a filesystem, use offset as partition start
-    //        static FlashIAPBlockDevice flashIAP_bd(0x8000000 + data_offset, 2 * 1024 * 1024 - data_offset);
-    //        return &flashIAP_bd;
-    //    } else {
-    //        // raw device, no offset
-    //        static FlashIAPBlockDevice flashIAP_bd(0x8000000, 2 * 1024 * 1024);
-    //        return &flashIAP_bd;
-    //    }
-    //}
-
-#if MCUBOOT_ENVIE_SDCARD
-    if (storage_type & SDCARD_FLAG) {
-        static SDMMCBlockDevice SDMMC_bd;
-        BOOT_LOG_INF("SDMMCBlockDevice");
-        return &SDMMC_bd;
-    }
-#endif
-
-    if (storage_type & QSPI_FLASH_FLAG) {
-        static QSPIFBlockDevice QSPIF_bd(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
-        BOOT_LOG_INF("QSPIFBlockDevice");
-        return &QSPIF_bd;
-    }
-
-    return NULL;
-#else //MCUBOOT_AS_ENVIE
 #if COMPONENT_SPIF
 
     static SPIFBlockDevice default_bd;
@@ -128,8 +68,6 @@ BlockDevice *BlockDevice::get_default_instance()
     return NULL;
 
 #endif
-
-#endif //MCUBOOT_AS_ENVIE
 }
 
 /**
@@ -139,151 +77,15 @@ BlockDevice *BlockDevice::get_default_instance()
 mbed::BlockDevice* get_secondary_bd(void) {
     // In this case, our flash is much larger than a single image so
     // slice it into the size of an image slot
-#if MCUBOOT_AS_ENVIE
     mbed::BlockDevice* default_bd = mbed::BlockDevice::get_default_instance();
-    mbed::BlockDevice* logical_bd;
-    storageType storage_type;
-    uint32_t data_offset;
-    uint32_t update_size;
-
-    getOTAData(&storage_type, &data_offset, &update_size);
-
-    if (storage_type & MBR_FLAG)  {
-        logical_bd = new MBRBlockDevice(default_bd, data_offset);
-        int err = logical_bd->init();
-        if (err) {
-            BOOT_LOG_ERR("Error initializing secondary mbr device");
-        }
-#if MCUBOOT_ENVIE_LITTLEFS
-        if((storage_type & LITTLEFS_FLAG)) {
-            static LittleFileSystem secondary_bd_fs("secondary");
-            err = secondary_bd_fs.mount(logical_bd);
-            if (err) {
-                BOOT_LOG_ERR("Error mounting littlefs on secondary mbr device");
-            }
-        } else {
-#endif
-            static FATFileSystem secondary_bd_fs("secondary");
-            err = secondary_bd_fs.mount(logical_bd);
-            if (err) {
-                BOOT_LOG_ERR("Error mounting fatfs on secondary mbr device");
-            }
-#if MCUBOOT_ENVIE_LITTLEFS
-        }
-#endif
-
-        static mbed::FileBlockDevice file_bd("/secondary/update.bin", "rb+", update_size);
-        return &file_bd;
-    } else {
-        int err = default_bd->init();
-        if (err) {
-            BOOT_LOG_ERR("Error initializing secondary raw device");
-        }
-#if MCUBOOT_ENVIE_LITTLEFS
-         if((storage_type & LITTLEFS_FLAG)) {
-            static LittleFileSystem secondary_bd_fs("secondary");
-            err = secondary_bd_fs.mount(default_bd);
-            if (err) {
-                BOOT_LOG_ERR("Error mounting littlefs on secondary raw device");
-            }
-        } else {
-#endif
-            static FATFileSystem secondary_bd_fs("secondary");
-            err = secondary_bd_fs.mount(default_bd);
-            if (err) {
-                BOOT_LOG_ERR("Error mounting fatfs on secondary raw device");
-            }
-#if MCUBOOT_ENVIE_LITTLEFS
-        }
-#endif
-
-        static mbed::FileBlockDevice file_bd("/secondary/update.bin", "rb+", update_size);
-        return &file_bd;
-    }
-
-    return NULL;
-#else
-    mbed::BlockDevice* default_bd = mbed::BlockDevice::get_default_instance();
-#if !defined MCUBOOT_USE_FILE_BD
     static mbed::SlicingBlockDevice sliced_bd(default_bd, 0x0, MCUBOOT_SLOT_SIZE);
     return &sliced_bd;
-#else
-    static mbed::MBRBlockDevice mbr_bd(default_bd, 2);
-
-    int err = mbr_bd.init();
-    if (err) {
-        BOOT_LOG_ERR("Error initializing secondary mbr device");
-    }
-
-    static mbed::FATFileSystem secondary_bd_fs("secondary");
-    err = secondary_bd_fs.mount(&mbr_bd);
-    if (err) {
-        BOOT_LOG_ERR("Error mounting fatfs on secondary mbr device");
-    }
-    static mbed::FileBlockDevice file_bd("/secondary/update.bin", "rb+", MCUBOOT_SLOT_SIZE);
-    return &file_bd;
-#endif
-#endif //MCUBOOT_AS_ENVIE
-
-
 }
 
 mbed::BlockDevice* get_scratch_bd(void) {
-
-#if MCUBOOT_AS_ENVIE
-    storageType storage_type;
-    uint32_t data_offset;
-    uint32_t update_size;
-
-    getOTAData(&storage_type, &data_offset, &update_size);
-
-    /*
-     * If the secondary slot is not into the internal QSPI flash, i.e. SDMCC; initialize
-     * QSPI storage and mount the filesystem.
-     *
-     * WARNING: by default we are assuming the QSPI flash is formatted as MBR device with FAT
-     *
-     */
-
-    //if(!(storage_type & QSPI_FLASH_FLAG)) {
-        mbed::BlockDevice* default_bd = new QSPIFBlockDevice(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
-        mbed::BlockDevice* logical_bd = new MBRBlockDevice(default_bd, 2);
-
-        int err = logical_bd->init();
-        if (err) {
-            BOOT_LOG_ERR("Error initializing scratch mbr device");
-        }
-
-        static mbed::FATFileSystem secondary_bd_fs("scratch");
-        err = secondary_bd_fs.mount(logical_bd);
-        if (err) {
-            BOOT_LOG_ERR("Error mounting fatfs on scratch mbr device");
-        }
-    //}
-
-    static mbed::FileBlockDevice file_bd("/scratch/scratch.bin", "rb+", MCUBOOT_SCRATCH_SIZE);
-    return &file_bd;
-#else
-#if !defined MCUBOOT_USE_FILE_BD
     static FlashIAPBlockDevice scratch_bd(MCUBOOT_SCRATCH_START_ADDR, MCUBOOT_SCRATCH_SIZE);
     return &scratch_bd;
-#else
-    mbed::BlockDevice* default_bd = mbed::BlockDevice::get_default_instance();
-    static mbed::MBRBlockDevice mbr_bd(default_bd, 2);
-
-    int err = mbr_bd.init();
-    if (err) {
-        BOOT_LOG_ERR("Error initializing scratch mbr device");
-    }
-
-    static mbed::FATFileSystem secondary_bd_fs("scratch");
-    err = secondary_bd_fs.mount(&mbr_bd);
-    if (err) {
-        BOOT_LOG_ERR("Error mounting fatfs on scratch mbr device");
-    }
-    static mbed::FileBlockDevice file_bd("/scratch/scratch.bin", "rb+", MCUBOOT_SCRATCH_SIZE);
-    return &file_bd;
-#endif
-#endif //MCUBOOT_AS_ENVIE
 }
+#endif //MCUBOOT_AS_ENVIE
+
 
--- envie/ota.cpp
+++ envie/ota.cpp
@@ -4,40 +4,206 @@
 #include "rtc.h"
 #include "bootutil/bootutil_log.h"
 
+#include "BlockDevice.h"
+#include "SlicingBlockDevice.h"
+#include "FlashIAPBlockDevice.h"
+#include "QSPIFBlockDevice.h"
+#include "SDMMCBlockDevice.h"
+#include "MBRBlockDevice.h"
+#include "FileBlockDevice.h"
+#include "FATFileSystem.h"
+#include "bootutil/bootutil_log.h"
+
+#if MCUBOOT_ENVIE_LITTLEFS
+#include "LittleFileSystem.h"
+#endif
+
 #define SD_OTA_TEST 1
 
 void getOTAData(enum storageType* storage_type, uint32_t* data_offset, uint32_t* update_size) {
-  RTCInit();
-  /*
-   * Magic 0x07AA is set by Arduino_Portenta_OTA
-   * Magic 0xDF59 is set by the loader if RESET_REASON_PIN_RESET
-   */
+    RTCInit();
+    /*
+     * Magic 0x07AA is set by Arduino_Portenta_OTA
+     * Magic 0xDF59 is set by the loader if RESET_REASON_PIN_RESET
+     */
 
 #if SD_OTA_TEST
-  *storage_type = SD_FATFS;
-  *data_offset = 2;
-  *update_size = MCUBOOT_SLOT_SIZE;
-  return;
-#endif
-
-  int magic = RTCGetBKPRegister(RTC_BKP_DR0);
-  if (magic == 0x07AA) {
-    // DR1 contains the backing storage type, DR2 the offset in case of raw device / MBR
-    *storage_type = (storageType)RTCGetBKPRegister(RTC_BKP_DR1);
-    *data_offset = RTCGetBKPRegister(RTC_BKP_DR2);
-    *update_size = RTCGetBKPRegister(RTC_BKP_DR3);
-    //BOOT_LOG_INF("Custom OTA data");
-  } else {
-    *storage_type = QSPI_FLASH_FATFS_MBR;
+    *storage_type = SD_FATFS;
     *data_offset = 2;
     *update_size = MCUBOOT_SLOT_SIZE;
-    //BOOT_LOG_INF("Default OTA data");
-  }
-  return;
+    return;
+#endif
 
-  //BOOT_LOG_INF("Storage type %d",(int)*storage_type);
-  //BOOT_LOG_INF("Offset %d",*data_offset);
-  //BOOT_LOG_INF("Update size %d",*update_size);
+    int magic = RTCGetBKPRegister(RTC_BKP_DR0);
+    if (magic == 0x07AA) {
+        // DR1 contains the backing storage type, DR2 the offset in case of raw device / MBR
+        *storage_type = (storageType)RTCGetBKPRegister(RTC_BKP_DR1);
+        *data_offset = RTCGetBKPRegister(RTC_BKP_DR2);
+        *update_size = RTCGetBKPRegister(RTC_BKP_DR3);
+        //BOOT_LOG_INF("Custom OTA data");
+    } else {
+        *storage_type = QSPI_FLASH_FATFS_MBR;
+        *data_offset = 2;
+        *update_size = MCUBOOT_SLOT_SIZE;
+        //BOOT_LOG_INF("Default OTA data");
+    }
+    return;
+
+    //BOOT_LOG_INF("Storage type %d",(int)*storage_type);
+    //BOOT_LOG_INF("Offset %d",*data_offset);
+    //BOOT_LOG_INF("Update size %d",*update_size);
+}
+
+/*
+ * MCUBOOT_AS_ENVIE     -> Secondary Block device defined by getOTAData [SDCARD, QSPI] Internal flash not supported
+ *                      -> Scratch Block device defined by getOTAData [SDCARD, QSPI, FLASH]
+ *
+ *                      -> Secondary Block device on RAW QSPI @0x0000000
+ *                      -> Scratch Block device on Internal flash @MCUBOOT_SCRATCH_START_ADDR
+ */
+
+BlockDevice *BlockDevice::get_default_instance()
+{
+    storageType storage_type;
+    uint32_t data_offset;
+    uint32_t update_size;
+
+    getOTAData(&storage_type, &data_offset, &update_size);
+
+    /* Used only by Scratch block device */
+    if (storage_type & INTERNAL_FLASH_FLAG) {
+        if (storage_type & (FATFS_FLAG | LITTLEFS_FLAG)) {
+            // have a filesystem, use offset as partition start
+            static FlashIAPBlockDevice flashIAP_bd(0x8000000 + data_offset, 2 * 1024 * 1024 - data_offset);
+            return &flashIAP_bd;
+        } else {
+            // raw device, no offset
+            static FlashIAPBlockDevice flashIAP_bd(0x8000000, 2 * 1024 * 1024);
+            return &flashIAP_bd;
+        }
+    }
+
+#if MCUBOOT_ENVIE_SDCARD
+    if (storage_type & SDCARD_FLAG) {
+        static SDMMCBlockDevice SDMMC_bd;
+        BOOT_LOG_INF("SDMMCBlockDevice");
+        return &SDMMC_bd;
+    }
+#endif
+
+    if (storage_type & QSPI_FLASH_FLAG) {
+        static QSPIFBlockDevice QSPIF_bd(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
+        BOOT_LOG_INF("QSPIFBlockDevice");
+        return &QSPIF_bd;
+    }
+
+    return NULL;
+}
+
+/**
+ * You can override this function to suit your hardware/memory configuration
+ * By default it simply returns what is returned by BlockDevice::get_default_instance();
+ */
+mbed::BlockDevice* get_secondary_bd(void) {
+    // In this case, our flash is much larger than a single image so
+    // slice it into the size of an image slot
+    mbed::BlockDevice* default_bd = mbed::BlockDevice::get_default_instance();
+    mbed::BlockDevice* logical_bd;
+    storageType storage_type;
+    uint32_t data_offset;
+    uint32_t update_size;
+
+    getOTAData(&storage_type, &data_offset, &update_size);
+
+    if (storage_type & MBR_FLAG)  {
+        logical_bd = new MBRBlockDevice(default_bd, data_offset);
+        int err = logical_bd->init();
+        if (err) {
+            BOOT_LOG_ERR("Error initializing secondary mbr device");
+        }
+#if MCUBOOT_ENVIE_LITTLEFS
+        if((storage_type & LITTLEFS_FLAG)) {
+            static LittleFileSystem secondary_bd_fs("secondary");
+            err = secondary_bd_fs.mount(logical_bd);
+            if (err) {
+                BOOT_LOG_ERR("Error mounting littlefs on secondary mbr device");
+            }
+        } else {
+#endif
+            static FATFileSystem secondary_bd_fs("secondary");
+            err = secondary_bd_fs.mount(logical_bd);
+            if (err) {
+                BOOT_LOG_ERR("Error mounting fatfs on secondary mbr device");
+            }
+#if MCUBOOT_ENVIE_LITTLEFS
+        }
+#endif
+
+        static mbed::FileBlockDevice file_bd("/secondary/update.bin", "rb+", update_size);
+        return &file_bd;
+    } else {
+        int err = default_bd->init();
+        if (err) {
+            BOOT_LOG_ERR("Error initializing secondary raw device");
+        }
+#if MCUBOOT_ENVIE_LITTLEFS
+         if((storage_type & LITTLEFS_FLAG)) {
+            static LittleFileSystem secondary_bd_fs("secondary");
+            err = secondary_bd_fs.mount(default_bd);
+            if (err) {
+                BOOT_LOG_ERR("Error mounting littlefs on secondary raw device");
+            }
+        } else {
+#endif
+            static FATFileSystem secondary_bd_fs("secondary");
+            err = secondary_bd_fs.mount(default_bd);
+            if (err) {
+                BOOT_LOG_ERR("Error mounting fatfs on secondary raw device");
+            }
+#if MCUBOOT_ENVIE_LITTLEFS
+        }
+#endif
+
+        static mbed::FileBlockDevice file_bd("/secondary/update.bin", "rb+", update_size);
+        return &file_bd;
+    }
+
+    return NULL;
+}
+
+mbed::BlockDevice* get_scratch_bd(void) {
+    storageType storage_type;
+    uint32_t data_offset;
+    uint32_t update_size;
+
+    getOTAData(&storage_type, &data_offset, &update_size);
+
+    /*
+     * If the secondary slot is not into the internal QSPI flash, i.e. SDMCC; initialize
+     * QSPI storage and mount the filesystem.
+     *
+     * WARNING: by default we are assuming the QSPI flash is formatted as MBR device with FAT
+     *
+     */
+
+
+        mbed::BlockDevice* default_bd = new QSPIFBlockDevice(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
+        mbed::BlockDevice* logical_bd = new MBRBlockDevice(default_bd, 2);
+
+        int err = logical_bd->init();
+        if (err) {
+            BOOT_LOG_ERR("Error initializing scratch mbr device");
+        }
+
+        static mbed::FATFileSystem secondary_bd_fs("scratch");
+        err = secondary_bd_fs.mount(logical_bd);
+        if (err) {
+            BOOT_LOG_ERR("Error mounting fatfs on scratch mbr device");
+        }
+
+
+    static mbed::FileBlockDevice file_bd("/scratch/scratch.bin", "rb+", MCUBOOT_SCRATCH_SIZE);
+    return &file_bd;
 }
 
 #endif
