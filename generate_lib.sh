6593eaed4c71bbbfcf0fe2675e94b6cab3575854
pennam <m.pennasilico@arduino.cc>
2022-03-18 12:03:45 +0100

    Update generate_lib sketch to use new json file

--- generate_lib.sh
+++ generate_lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf BUILD/PORTENTA_H7_M7/GCC_ARM/
-mbed compile -m PORTENTA_H7_M7 -t GCC_ARM 
+mbed compile -m PORTENTA_H7_M7 -t GCC_ARM --app=mbed_app_log_off.json
 cd BUILD/PORTENTA_H7_M7/GCC_ARM/
 rm -rf mbed-os/
 find . -name "*.o" | xargs arm-none-eabi-ar -csr libbootutil.a
