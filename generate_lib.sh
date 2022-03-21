2e6e82848149717e879c60d51907f92ac08bd2ba
pennam <m.pennasilico@arduino.cc>
2022-03-21 13:09:35 +0100

    Improve generate_lib script
      Use new json file mbed_app_bootutil
      Use mbed compile -c to rebuild everything from scratch
      Include only needed *.o files
      Print libbootutil path at the end of build process

--- generate_lib.sh
+++ generate_lib.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm -rf BUILD/PORTENTA_H7_M7/GCC_ARM/
-mbed compile -m PORTENTA_H7_M7 -t GCC_ARM --app=mbed_app_log_off.json
-cd BUILD/PORTENTA_H7_M7/GCC_ARM/
-rm -rf mbed-os/
-find . -name "*.o" | xargs arm-none-eabi-ar -csr libbootutil.a
+mbed compile -c -m PORTENTA_H7_M7 -t GCC_ARM --app=mbed_app_bootutil.json
+echo
+echo Generating library
+find ./BUILD/PORTENTA_H7_M7/GCC_ARM/ \( -name "FileBlockDevice.o" -o -name "rtc.o" -o -name "ota.o" -o -name "bootutil_extra.o" -o -name "flash_map_backend.o" -o -name "bootutil_public.o" \) | xargs arm-none-eabi-ar -csr libbootutil.a
+echo -n "Library: "
+find ./ -name "libbootutil.a"
+echo
