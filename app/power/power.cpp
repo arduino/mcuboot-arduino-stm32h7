8d6e1435a14589d13a08de722b9e0089b6d84149
bogdanarduino <b.ivanus@arduino.cc>
2025-09-02 15:56:32 +0300

    Update app/power/power.cpp
    Co-authored-by: Sebastian Romero <s.romero.zh@gmail.com>

--- app/power/power.cpp
+++ app/power/power.cpp
@@ -102,7 +102,7 @@ static void portenta_power_init() {
   data[1]=0x7;
   i2c.write(8 << 1, data, sizeof(data));
   
-  // SW1 turn back ON (SW1_VOLT_CTRL)
+  // SW1 turn back ON (SW1_CTRL)
   data[0]=0x35;
   data[1]=0xF;
   i2c.write(8 << 1, data, sizeof(data));
